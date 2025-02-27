cf2530a8bec1f883b6f9840f72e3890d2b61e91f
Pargat Singh <Pargat@eprofileverified.com>
2025-02-27 18:35:37 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('push', ()=>{
-    self.registration.sendNotification('Test Notification', {});
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'this notification was generated from a psuh!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions:[
+            {
+                action: 'explore', 
+                title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                ction: 'close', 
+                title: 'Close',
+                icon: 'images/xmark.png'
+            }
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Hello World', options)
+    );
 });
